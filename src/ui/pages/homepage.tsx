26fd568f43d1938fe7ca7dc951794e5b54a47a78
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-14 02:27:43 +0000

    chore:

--- src/ui/pages/homepage.tsx
+++ src/ui/pages/homepage.tsx
@@ -7,6 +7,15 @@ const Homepage = () => {
       <Link to="/private/dashboard">To Dashboard</Link>
       <Link to="/private/compliance">To Compliance</Link>
       <Link to="/private/invoice">To Invoice</Link>
+      <p className="font-[100]"> Aa 123</p>
+      <p className="font-[200]"> Aa 123</p>
+      <p className="font-[300]"> Aa 123</p>
+      <p className="font-[450] italic"> Italic</p>
+      <p className="font-[550]"> 550</p>
+      <p className="font-[600]"> Aa 123</p>
+      <p className="font-[700]"> Aa 123</p>
+      <p className="font-[800]"> Aa 123</p>
+      <p className="font-[900]"> Aa 123</p>
     </div>
   );
 };
