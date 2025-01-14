9137c8b6890244373a487012b160a6a094f00836
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-14 16:45:21 +0000

    feat: implet Dashboard UI context

--- src/ui/components/common/ui-context-checker.tsx
+++ src/ui/components/common/ui-context-checker.tsx
@@ -1,11 +1,25 @@
 import { useDashboardUIContext } from "../../../custom-hooks/use-dashboard-ui-context";
 
 const UIContextChecker = () => {
-  const { DashboardUIContext } = useDashboardUIContext();
+  const { DashboardUIContext, setDashboardUIContext } = useDashboardUIContext();
+
+  const handleOnClick = () => {
+    const updatedContext = {
+      ...DashboardUIContext,
+      isRevenueGraphVisible: !DashboardUIContext.isRevenueGraphVisible,
+      isInvoicesGraphVisible: !DashboardUIContext.isInvoicesGraphVisible,
+      isUsersGraphVisible: !DashboardUIContext.isUsersGraphVisible,
+      isDerivedCardVisible: !DashboardUIContext.isDerivedCardVisible,
+      areGraphsCombined: !DashboardUIContext.areGraphsCombined,
+    };
+
+    setDashboardUIContext(updatedContext);
+  };
 
   return (
     <div>
       <pre>{JSON.stringify(DashboardUIContext, null, 2)}</pre>
+      <button onClick={handleOnClick}>Invert</button>
     </div>
   );
 };
