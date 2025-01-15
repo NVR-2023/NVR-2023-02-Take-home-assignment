140bfe736e540ddd3d681ade5f55a0171947b052
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-15 02:01:37 +0000

    feat: add form context

--- src/ui/pages/invoicer.tsx
+++ src/ui/pages/invoicer.tsx
@@ -1,10 +1,13 @@
 import InvoiceIssuerDataProvider from "../../contexts/invoicer-issuer/invoice-issuer-data-provider";
 import InvoiceClientsDataProvider from "../../contexts/invoicer-clients/invocie-clients-data-provider";
 import InvoicerProductsDataProvider from "../../contexts/invoicer-products/invocier-products-data-provider";
+import { InvoiceFormContextProvider } from "../../contexts/invoicer-form/invoice-form-context-provider";
 
 import { useInvoiceIssuerContext } from "../../custom-hooks/use-invoice-issuer-context";
 import { useInvoiceClientsContext } from "../../custom-hooks/use-invoice-clients-context";
 import { useInvoiceProductsContext } from "../../custom-hooks/use-invoice-products-context";
+
+import UIContextChecker3 from "../components/common/ui-context-checker-3";
 
 const Invoicer = () => {
   const {
@@ -42,18 +45,21 @@ const Invoicer = () => {
       <pre>{JSON.stringify(clientsData, null, 2)}</pre>
       <p>Products Data:</p>
       <pre>{JSON.stringify(productsData, null, 2)}</pre>
+      <UIContextChecker3 />
     </div>
   );
 };
 
 const InvoicerWithContexts = () => (
-  <InvoicerProductsDataProvider>
-    <InvoiceClientsDataProvider>
-      <InvoiceIssuerDataProvider>
-        <Invoicer />
-      </InvoiceIssuerDataProvider>
-    </InvoiceClientsDataProvider>
-  </InvoicerProductsDataProvider>
+  <InvoiceFormContextProvider>
+    <InvoicerProductsDataProvider>
+      <InvoiceClientsDataProvider>
+        <InvoiceIssuerDataProvider>
+          <Invoicer />
+        </InvoiceIssuerDataProvider>
+      </InvoiceClientsDataProvider>
+    </InvoicerProductsDataProvider>
+  </InvoiceFormContextProvider>
 );
 
 export default InvoicerWithContexts;
