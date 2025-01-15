7cf62bb93c3cd64d2f03be07c4d4a77a53ce6ee8
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-15 21:40:08 +0000

    chore:

--- src/ui/sections/sidebar/sidebar.tsx
+++ src/ui/sections/sidebar/sidebar.tsx
@@ -1,15 +1,13 @@
-import AccountSegment from "./sub-components/account-segment";
 import LinksSegment from "./sub-components/links-segment";
-import SettingsSegment from "./sub-components/settings-segment";
+import AccountSegment from "./sub-components/account-segment";
 
 const Sidebar = () => {
   return (
     <nav className="w-9 pt-12 pb-4 px-2 flex flex-col justify-between overflow-x-clip duration-300 transition-all hover:w-30 min-h-full h-full rounded bg-zinc-200">
       <div>
-        <AccountSegment />
         <LinksSegment />
       </div>
-      <SettingsSegment />
+      <AccountSegment />
     </nav>
   );
 };
--- src/ui/sections/sidebar/sub-components/account-segment.tsx
+++ src/ui/sections/sidebar/sub-components/account-segment.tsx
@@ -1,9 +1,15 @@
+import SettingsIcon from "../../../components/icons/settings-icon";
+import LinkWithHideableTag from "../../../components/common/link-with-hideable-tag";
+import LogoIcon from "../../../components/icons/logo-icon";
 const AccountSegment = () => {
   return (
-    <div >
-      A
+    <div className="flex flex-col space-y-6">
+      <LinkWithHideableTag Icon={SettingsIcon} text="Settings" url="/" />
+      <div>
+        <LogoIcon scale={0.9} color="#3f3f46" />
+      </div>
     </div>
-  )
+  );
 };
 
 export default AccountSegment;
