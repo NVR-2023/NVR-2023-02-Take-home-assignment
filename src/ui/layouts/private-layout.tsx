f078697da089e153b0e7fc2790b2a973ac3af3fd
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-15 13:22:01 +0000

    chore:

--- src/ui/layouts/private-layout.tsx
+++ src/ui/layouts/private-layout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../sections/sidebar/sidebar";
+import MobileNavbar from "../sections/mobile-navbar/mobile-navbar";
 import Footer from "../sections/footer/footer";
 
 const PrivateLayout = () => {
   return (
-    <div className="private-layout overflow-x-clip">
-      <div className="p-4 flex flex-col lg:flex-row gap-4 max-h-screen bg-yellow-400">
-        <aside className="overflow-y-hidden sm:hidden lg:block" role="navigation">
+    <div className="private-layout overflow-x-hidden">
+      <div className="p-4 flex flex-col lg:flex-row gap-4 max-h-screen h-screen bg-yellow-400">
+        <aside
+          className="border-4 border-blue-500 overflow-y-hidden hidden lg:block h-full"
+          role="navigation">
           <Sidebar />
         </aside>
-        <main className="overflow-y-hidden flex-grow" role="main">
+        <main className="border-4 border-blue-500 overflow-y-hidden flex-grow h-full" role="main">
+          <div className="md:hidden" role="navigation">
+            <MobileNavbar />
+          </div>
           <Outlet />
         </main>
       </div>
---
+++ src/ui/sections/mobile-navbar/mobile-navbar.tsx
@@ -0,0 +1,5 @@
+const MobileNavbar = () => {
+  return <div>This mobile Navbar</div>;
+};
+
+export default MobileNavbar;
