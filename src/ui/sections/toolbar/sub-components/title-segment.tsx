1a0deb488aa9915f61d9b3085adc1e2771ec3466
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-15 17:58:30 +0000

    chore:

--- src/ui/sections/toolbar/sub-components/title-segment.tsx
+++ src/ui/sections/toolbar/sub-components/title-segment.tsx
@@ -3,7 +3,7 @@ import LogoIcon from "../../../components/icons/logo-icon";
 
 const TitleSegment = ({ title }: { title: string }) => {
   return (
-    <div className="flex space-x-1">
+    <div className="flex space-x-1.5">
       <span className="hidden md:block transform translate-y-1">
         <Link to="/private/dashboard">
           <LogoIcon scale={0.55} color={"#a1a1aa"} />
