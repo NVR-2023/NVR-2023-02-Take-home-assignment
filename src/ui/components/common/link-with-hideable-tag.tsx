b3a06d62d1bcaa4489bd7f305a6741a12834e436
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-15 21:18:26 +0000

    chore:

--- src/ui/components/common/link-with-hideable-tag.tsx
+++ src/ui/components/common/link-with-hideable-tag.tsx
@@ -5,11 +5,11 @@ const LinkWithHideableTag = ({ Icon, text, url }: LinkWithHideableTagProps) => {
   return (
     <Link
       to={url}
-      className="h-5 group flex items-center space-x-3 duration-150 transition-all hover:bg-zinc-300 rounded">
+      className="h-5 w-27 group flex items-center space-x-3 duration-150 transition-all hover:bg-zinc-300 rounded">
       <span className=" transform translate-x-0.5 ">
         <Icon scale={0.75} color="#3f3f46" />
       </span>
-      <span className="text-xs font-medium group-hover:font-bold text-zinc-700 transition-all duration-300 ease-in-out">
+      <span className="text-xs hidden md:inline font-medium group-hover:font-bold text-zinc-700 transition-all duration-300 ease-in-out">
         {text}
       </span>
     </Link>
--- src/ui/sections/sidebar/sub-components/links-segment.tsx
+++ src/ui/sections/sidebar/sub-components/links-segment.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import HomepageIcon from "../../../components/icons/homepage-icon";
 import DashboardIcon from "../../../components/icons/dashboard-icon";
 import ComplianceTrackerIcon from "../../../components/icons/complaince-tarcker-icon";
@@ -5,15 +7,41 @@ import InvoicerIcon from "../../../components/icons/invoicer-icon";
 import LinkWithHideableTag from "../../../components/common/link-with-hideable-tag";
 
 const LinksSegment = () => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+
+  const links = useMemo(
+    () => [
+      { Icon: DashboardIcon, text: "Dashboard", url: "/private/dashboard" },
+      { Icon: ComplianceTrackerIcon, text: "Tracker", url: "/private/compliance-tracker" },
+      { Icon: InvoicerIcon, text: "Invoicer", url: "/private/invoicer" },
+      { Icon: HomepageIcon, text: "Homepage", url: "/" },
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const activeLink = links.find((link) => currentUrl.includes(link.url));
+    setActiveLink(activeLink ? activeLink.url : null);
+  }, [currentUrl, links]);
+
   return (
-    <div className="flex flex-col">
-      <LinkWithHideableTag Icon={HomepageIcon} text="Homepage" url="/" />
-      <LinkWithHideableTag Icon={HomepageIcon} text="Dashboard" url="/dashboard" />
-      <LinkWithHideableTag Icon={HomepageIcon} text="Tracker" url="/" />
-      <LinkWithHideableTag Icon={InvoicerIcon} text="Invoicer" url="/invoicer" />
+    <ul className="flex flex-col space-y-5">
+      {links.map((link, index) => {
+        const isActive = activeLink === link.url;
 
+        return (
+          <li key={index} className="relative flex items-center">
+            {isActive && link.url !== "/" && (
+              <div className="absolute -left-1 top-0 h-full border-[1.5px] border-r-[#6868f6]" />
+            )}
 
-    </div>
+            <LinkWithHideableTag Icon={link.Icon} text={link.text} url={link.url} />
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
