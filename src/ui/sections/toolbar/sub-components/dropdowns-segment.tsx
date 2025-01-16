08b9f0c5a30ea1e444cf556f89a75c4e49190449
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-16 02:13:26 +0000

    chore:

--- src/ui/sections/toolbar/sub-components/dropdowns-segment.tsx
+++ src/ui/sections/toolbar/sub-components/dropdowns-segment.tsx
@@ -1,4 +1,3 @@
-import { DashboardUIContext } from "../../../../contexts/dashboard-ui/dashboard-ui-contex";
 import { useCombinedTimelinesContext } from "../../../../custom-hooks/use-combined-timelines-context";
 import { useDashboardUIContext } from "../../../../custom-hooks/use-dashboard-ui-context";
 import { useState, useEffect } from "react";
@@ -6,17 +5,29 @@ import { useState, useEffect } from "react";
 const DropdownsSegment = () => {
   const { data, isLoading, hasErrors } = useCombinedTimelinesContext();
   const { DashboardUIContext, setDashboardUIContext } = useDashboardUIContext();
-  const [datesSet, setDatesSet] = useState<string[]>([]);
+  const [startDatesSet, setStartDatesSet] = useState<string[]>([]);
+  const [endDatesSet, setEndDatesSet] = useState<string[]>([]);
 
   useEffect(() => {
     if (!isLoading && !hasErrors) {
       const dates: string[] = data.map((entry, key) => entry.date);
-      setDatesSet(dates);
+      setStartDatesSet(dates);
       const firstDate = dates[0];
       const lastDate = dates[dates.length - 1];
       setDashboardUIContext({ ...DashboardUIContext, startDate: firstDate, endDate: lastDate });
     }
   }, [data, isLoading, hasErrors]);
+
+  useEffect(() => {
+    const startDateIndex = startDatesSet.findIndex((date) => date === DashboardUIContext.startDate);
+    if (startDateIndex !== -1 && startDateIndex === startDatesSet.length - 1) {
+      setEndDatesSet([startDatesSet[startDateIndex]]);
+    } else if (startDateIndex !== -1) {
+      const filteredEndDatesSet = startDatesSet.slice(startDateIndex + 1);
+      setEndDatesSet(filteredEndDatesSet);
+    }
+  }, [DashboardUIContext.startDate, startDatesSet]);
+
   return <div className="flex items-center">Dropdowns</div>;
 };
 
