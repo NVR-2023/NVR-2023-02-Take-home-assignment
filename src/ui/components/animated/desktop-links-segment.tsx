bd19189bc171fcca5ae1e805b359e09eaef5716e
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-16 15:29:55 +0000

    chore:

--- src/ui/components/animated/desktop-links-segment.tsx
+++ src/ui/components/animated/desktop-links-segment.tsx
@@ -4,6 +4,28 @@ import { useLocation } from "react-router-dom";
 import DesktopLinkWithTag from "../common/desktop-link-with-tag";
 import { LinksSegmentProps } from "../../../types/global-types";
 
+const LinkGroup = ({
+  links,
+  activeLink,
+}: {
+  links: LinksSegmentProps["links"];
+  activeLink: string | null;
+}) => (
+  <div className="space-y-5">
+    {links.map((link, index) => (
+      <li key={`link-${index}`} className="relative flex items-center">
+        {activeLink === link.url && link.url !== "/" && (
+          <motion.div
+            layoutId="currentPageIndicator"
+            className="absolute -left-1 top-0 h-full border-[1.5px] border-r-[#6868f6]"
+          />
+        )}
+        <DesktopLinkWithTag Icon={link.Icon} text={link.text!} url={link.url} />
+      </li>
+    ))}
+  </div>
+);
+
 const DesktopLinksSegment = ({ links: desktopLinks, splitAfter }: LinksSegmentProps) => {
   const location = useLocation();
   const currentUrl = location.pathname;
@@ -21,32 +43,8 @@ const DesktopLinksSegment = ({ links: desktopLinks, splitAfter }: LinksSegmentPr
 
   return (
     <ul className="flex flex-col h-full justify-between">
-      <div className="space-y-5">
-        {desktopLinks.slice(0, splitAfter).map((link, index) => (
-          <li key={`first-${index}`} className="relative flex items-center">
-            {activeLink === link.url && link.url !== "/" && (
-              <motion.div
-                layoutId="currentPageIndicator"
-                className="absolute -left-1 top-0 h-full border-[1.5px] border-r-[#6868f6]"
-              />
-            )}
-            <DesktopLinkWithTag Icon={link.Icon} text={link.text!} url={link.url} />
-          </li>
-        ))}
-      </div>
-      <div className="space-y-5">
-        {desktopLinks.slice(splitAfter).map((link, index) => (
-          <li key={`second-${index}`} className="relative flex items-center">
-            {activeLink === link.url && link.url !== "/" && (
-              <motion.div
-                layoutId="currentPageIndicator"
-                className="absolute -left-1 top-0 h-full border-[1.5px] border-r-[#6868f6]"
-              />
-            )}
-            <DesktopLinkWithTag Icon={link.Icon} text={link.text!} url={link.url} />
-          </li>
-        ))}
-      </div>
+      <LinkGroup links={desktopLinks.slice(0, splitAfter)} activeLink={activeLink} />
+      <LinkGroup links={desktopLinks.slice(splitAfter)} activeLink={activeLink} />
     </ul>
   );
 };
