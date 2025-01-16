3a40ec850db1cdf7696d0ff08fb21b14740683ba
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-16 11:26:58 +0000

    chore:

--- src/services/api/api-services.ts
+++ src/services/api/api-services.ts
@@ -1,21 +1,21 @@
 import { fetchData } from "../../utils/fetch-data";
 
 export const fetchCombinedTimeseries = async () => {
-  return fetchData({ url: "/mock-data/mock-combined-timeseries.json" });
+  return await fetchData({ url: "/mock-data/mock-combined-timeseries.json" });
 };
 
 export const fetchComplianceStatus = async () => {
-  return fetchData({ url: "/mock-data/mock-compliance-status.json" });
+  return await fetchData({ url: "/mock-data/mock-compliance-status.json" });
 };
 
 export const fetchIssuerDetails = async () => {
-  return fetchData({ url: "/mock-data/mock-invoice-issuer-details.json" });
+  return await fetchData({ url: "/mock-data/mock-invoice-issuer-details.json" });
 };
 
 export const fetchClientsDetails = async () => {
-  return fetchData({ url: "/mock-data/mock-clients-details.json" });
+  return await fetchData({ url: "/mock-data/mock-clients-details.json" });
 };
 
 export const fetchProductsDetails = async () => {
-  return fetchData({ url: "/mock-data/mock-products-details.json" });
+  return await fetchData({ url: "/mock-data/mock-products-details.json" });
 };
