42df958466b96c0e6da3bd6aec372c54acde110c
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-16 20:05:56 +0000

    chore:

--- src/ui/sections/content-area/content-area.tsx
+++ src/ui/sections/content-area/content-area.tsx
@@ -2,8 +2,8 @@ import React, { ReactNode } from "react";
 
 const ContentArea = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="h-[calc(100vh-6.75rem)] w-full rounded bg-zinc-300 overflow-y-scroll">
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-full">
+    <div className="h-[calc(100vh-6.75rem)] p-2 w-full rounded bg-zinc-300 overflow-y-scroll">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-4 h-full">
         {React.Children.map(children, (child) => (
           <div className="border-2 border-blue-400 p-4">{child}</div>
         ))}
