43a55f0157e5e04581205989035702f957f92823
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-16 15:12:32 +0000

    chore:

--- src/ui/components/common/desktop-link-with-tag.tsx
+++ src/ui/components/common/desktop-link-with-tag.tsx
@@ -9,7 +9,7 @@ const DesktopLinkWithTag = ({ Icon, text, url }: LinkProps) => {
       <span className=" transform translate-x-0.25 ">
         <Icon scale={0.75} color="#3f3f46" />
       </span>
-      <span className="text-xs hidden md:inline font-medium group-hover:font-bold text-zinc-700 transition-all duration-300 ease-in-out">
+      <span className="text-xs font-medium group-hover:font-bold text-zinc-700 transition-all duration-250 ease-in-out">
         {text}
       </span>
     </Link>
