555a3b09eee90748536155b74faaac71b9f34191
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-17 00:25:01 +0000

    chore:

--- src/ui/animations/homepage-animations.ts
+++ src/ui/animations/homepage-animations.ts
@@ -1,30 +1,27 @@
 export const backgroundAnimation = {
-  initial: { opacity: 0.21 },
+  initial: { opacity: 0.7 },
   animate: { opacity: 1 },
-  exit: { opacity: 0, x: -100 },
   transition: {
-    duration: 1.5,
-    ease: [0.43, 0.13, 0.23, 0.96],
+    duration: 2.1,
+    ease: [0.16, 1, 0.3, 1],
   },
 };
 
 export const textLogoAnimation = {
-  initial: { opacity: 0, y: 20 },
+  initial: { opacity: 0.3, y: 20 },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, x: -100 },
   transition: {
     duration: 0.2,
-    ease: [0.43, 0.13, 0.23, 0.96],
+    ease: [0.16, 1, 0.3, 1],
   },
 };
 
 export const buttonAnimation = {
-  initial: { opacity: 0, y: 20 },
+  initial: { opacity: 0.3, y: 20 },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, x: -100 },
   transition: {
     duration: 0.25,
     delay: 0.025,
-    ease: [0.87, 0, 0.13, 1],
+    ease: [0.16, 1, 0.3, 1],
   },
 };
