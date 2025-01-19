e0df1a08faf5fccc2cd5791fbef79ed0eaceb9d0
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-19 03:44:46 +0000

    chore:

--- src/ui/components/charts/overview-card.tsx
+++ src/ui/components/charts/overview-card.tsx
@@ -1,6 +1,6 @@
 const OverviewCard = () => {
   return (
-    <div className="min-w-48 max-w-full min-h-30 w-50 h-full max-h-70' rounded bg-blue-300">
+    <div className="min-w-48 flex-grow max-w-full min-h-30 w-50 h-full max-h-70' rounded bg-blue-300">
       Card
     </div>
   )
--- src/ui/pages/dashboard/dashboard.tsx
+++ src/ui/pages/dashboard/dashboard.tsx
@@ -17,11 +17,19 @@ const Dashboard = () => {
   const { data } = useCombinedTimelinesContext();
   const ToolsArray = [DashboardSliderSegment, DashboardTogglesSegment];
 
-  
+  const { DashboardUIContext } = useDashboardUIContext();
+  const {
+    isDerivedCardVisible,
+    isInvoicesGraphVisible,
+    isRevenueGraphVisible,
+    isUsersGraphVisible,
+    areGraphsCombined,
+  } = DashboardUIContext;
+
   return (
     <motion.div
       {...pageAnimation}
-      className="bg-yellow-200 rounded h-auto md:min-h-screen md:h-screen p-4 space-y-2 md:overflow-x-clip">
+      className="bg-zinc-200 rounded h-auto md:min-h-screen md:h-screen p-4 space-y-2 md:overflow-x-clip">
       <Toolbar title="Dashboard" toolsArray={ToolsArray} />
       <ContentArea>
         <div className="w-full h-full grid grid-cols-1 gap-4 sm:grid-cols-3 sm:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1">
@@ -41,7 +49,7 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="bg-blue-500 p-4 rounded flex items-stretch sm:col-span-3 lg:col-span-1">
-            <div className="w-full h-full">
+            <div className="w-full h-full justify-center">
               <OverviewCard />
             </div>
           </div>
