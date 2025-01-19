7bfdc1553ba4954a8cc408a67f95336cfe876c7e
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-19 18:58:34 +0000

    chore:

--- src/ui/components/animated/card/close-card-button.tsx
+++ src/ui/components/animated/card/close-card-button.tsx
@@ -2,24 +2,19 @@ import { motion } from "framer-motion";
 import { buttonTapVariants } from "../../../animations/button-animations";
 import CloseIcon from "../../icons/close-icon";
 
-const CloseCardButton = ({
-  closeCardFunction,
-}: {
-  closeCardFunction: () => void; 
-}) => {
+const CloseCardButton = ({ closeCardFunction }: { closeCardFunction: () => void }) => {
   const handleOnClick = () => {
-    closeCardFunction(); 
+    closeCardFunction();
   };
 
   return (
-    <span>
-      <motion.button
-        {...buttonTapVariants}
-        onClick={handleOnClick}
-        className="rounded-[1.5px] bg-[#b4b9c1] transition-all duration-300 hover:bg-[#a2a8b1] text-zinc-500">
+    <motion.button
+      {...buttonTapVariants}
+      className="cursor-pointer flex items-center justify-center w-3 h-3 transform translate-y-0.5 rounded-[2px] bg-[#b4b9c1] transition-all duration-300 hover:bg-[#a2a8b1] text-zinc-500 cursorpointer">
+      <span onClick={handleOnClick}>
         <CloseIcon scale={0.5} />
-      </motion.button>
-    </span>
+      </span>
+    </motion.button>
   );
 };
 
