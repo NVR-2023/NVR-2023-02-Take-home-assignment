f0230bff2adbfd84e02d5e91f442f5537a2e4127
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-19 13:50:53 +0000

    chore:

--- src/ui/pages/dashboard/sub-components/dektop-layout.tsx
+++ src/ui/pages/dashboard/sub-components/dektop-layout.tsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useDashboardUIContext } from "../../../../custom-hooks/use-dashboard-ui-context";
-
+import OverviewCard from "../../../components/animated/overview-card";
 const DesktopLayout = () => {
   const { DashboardUIContext } = useDashboardUIContext();
   const {
@@ -11,10 +11,10 @@ const DesktopLayout = () => {
   } = DashboardUIContext;
 
   const columns = [
-    { isVisible: isDerivedCardVisible, color: "bg-green-500" },
-    { isVisible: isInvoicesGraphVisible, color: "bg-blue-500" },
-    { isVisible: isRevenueGraphVisible, color: "bg-red-500" },
-    { isVisible: isUsersGraphVisible, color: "bg-yellow-500" },
+    { isVisible: isDerivedCardVisible, card: <OverviewCard /> },
+    { isVisible: isInvoicesGraphVisible, card: <OverviewCard /> },
+    { isVisible: isRevenueGraphVisible, card: <OverviewCard /> },
+    { isVisible: isUsersGraphVisible, card: <OverviewCard /> },
   ];
 
   const visibleColumns = columns.filter((col) => col.isVisible);
@@ -22,16 +22,16 @@ const DesktopLayout = () => {
 
   return (
     <div className="flex w-full h-full gap-3">
-      {columns.map((col, index) => (
+      {columns.map((column, index) => (
         <AnimatePresence key={index}>
-          {col.isVisible && (
+          {column.isVisible && (
             <motion.div
-              className={`${col.color} overflow-hidden`}
-              initial={{ width: 0 }}
+              className="overflow-hidden"
+              initial={{ width: columnWidth }}
               animate={{ width: columnWidth }}
               exit={{ width: 0 }}
               transition={{ duration: 0.18, ease: "easeInOut" }}>
-              {`card ${index + 1}`}
+              {column.card}
             </motion.div>
           )}
         </AnimatePresence>
--- src/ui/sections/sidebar/sidebar.tsx
+++ src/ui/sections/sidebar/sidebar.tsx
@@ -2,7 +2,7 @@ import LinksSegment from "./sub-components/links-segment";
 
 const Sidebar = () => {
   return (
-    <nav className="overflow-x-clip w-9 pt-16 pb-4 px-2 flex flex-col justify-between space-y-2  duration-300 transition-all hover:w-30 h-full rounded bg-zinc-200">
+    <nav className="overflow-x-clip w-9 pt-14 pb-4 px-2 flex flex-col justify-between space-y-2  duration-300 transition-all hover:w-30 h-full rounded bg-zinc-200">
       <LinksSegment />
     </nav>
   );
