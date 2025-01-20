031f730c5c88c32263cbe10e0f91378c8742010f
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-20 15:01:03 +0000

    chore:

--- src/ui/pages/dashboard/sub-components/revenue-chart-card.tsx
+++ src/ui/pages/dashboard/sub-components/revenue-chart-card.tsx
@@ -5,11 +5,14 @@ import { cardAnimation } from "../../../animations/card-animation";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 import { shortenDateString } from "../../../../utils/shoten-date-string";
 import useFilteredDashboardData from "../../../../custom-hooks/use-filtered-data";
+import LoadingIndicator from "../../../components/animated/loading-indicator";
+import { useCombinedTimelinesContext } from "../../../../custom-hooks/use-combined-timelines-context";
 
 const RevenueChartCard = () => {
   const { DashboardUIContext, setDashboardUIContext } = useDashboardUIContext();
   const { isRevenueGraphVisible } = DashboardUIContext;
   const data = useFilteredDashboardData();
+  const { isLoading } = useCombinedTimelinesContext();
 
   const handleOnCloseDerivedCard = () => {
     setDashboardUIContext((previousContext) => ({
@@ -29,62 +32,71 @@ const RevenueChartCard = () => {
               handleOnCloseDerivedCard();
             }
           }}>
-          <div className="rounded bg-[#ccccd0] flex flex-col w-full h-full p-2">
-            <CardHeaderSegment title="Revenue" closeFunction={handleOnCloseDerivedCard} />
-
-            <div
-              className="flex flex-grow h-full justify-center items-center"
-              style={{ backgroundColor: "#ccccd0", padding: 0, margin: 0 }}>
-              <ResponsiveContainer width="100%" height="100%" className=" transform -translate-x-[20%]">
-                <BarChart data={data} className="">
-                  <defs>
-                    <linearGradient id="revenueGradient" x1="0" y1="1" x2="0" y2="0">
-                      <stop offset="0%" stopColor="#b0b0ee" />
-                      <stop offset="100%" stopColor="#4B4BD8" />
-                    </linearGradient>
-                  </defs>
-                  <XAxis
-                    dataKey="date"
-                    tick={{ fill: "#3f3f46", fontSize: 9, fontWeight: 600, }}
-                    axisLine={false}
-                    tickLine={false}
-                    tickFormatter={(value) => shortenDateString(value)}
-                  />
-
-                  <YAxis
-                    tick={{
-                      fill: "#3f3f46",
-                      fontSize: 9,
-                      fontWeight: 600,
-                    }}
-                    axisLine={false}
-                    tickLine={false}
-                    tickFormatter={(value) => {
-                      if (value >= 1000) return `${value / 1000}k`;
-                      return value.toString();
-                    }}
-                    className="m-0 p-0"
-                  />
-                  <Tooltip
-                    contentStyle={{
-                      backgroundColor: "rgba(204, 204, 208, 0.7)",
-                      borderRadius: "2px",
-                      fontSize: "9px",
-                      fontWeight: 600,
-                      width: "90px",
-                      height: "30px",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                    }}
-                    labelStyle={{ color: "#1f2937" }}
-                    formatter={(value) => [value, null]}
-                  />
-                  <Bar dataKey="revenue" fill="url(#revenueGradient)" radius={[1, 1, 0, 0]} />
-                </BarChart>
-              </ResponsiveContainer>
+          {isLoading ? (
+            <div className="flex justify-center items-center h-full">
+              <LoadingIndicator />
             </div>
-          </div>
+          ) : (
+            <div className="rounded bg-[#ccccd0] flex flex-col w-full h-full p-2">
+              <CardHeaderSegment title="Revenue" closeFunction={handleOnCloseDerivedCard} />
+
+              <div
+                className="flex flex-grow h-full justify-center items-center"
+                style={{ backgroundColor: "#ccccd0", padding: 0, margin: 0 }}>
+                <ResponsiveContainer
+                  width="100%"
+                  height="100%"
+                  className=" transform scale-105 -translate-x-5">
+                  <BarChart data={data} className="">
+                    <defs>
+                      <linearGradient id="revenueGradient" x1="0" y1="1" x2="0" y2="0">
+                        <stop offset="0%" stopColor="#b0b0ee" />
+                        <stop offset="100%" stopColor="#4B4BD8" />
+                      </linearGradient>
+                    </defs>
+                    <XAxis
+                      dataKey="date"
+                      tick={{ fill: "#3f3f46", fontSize: 9, fontWeight: 600 }}
+                      axisLine={false}
+                      tickLine={false}
+                      tickFormatter={(value) => shortenDateString(value)}
+                    />
+
+                    <YAxis
+                      tick={{
+                        fill: "#3f3f46",
+                        fontSize: 9,
+                        fontWeight: 600,
+                      }}
+                      axisLine={false}
+                      tickLine={false}
+                      tickFormatter={(value) => {
+                        if (value >= 1000) return `${value / 1000}k`;
+                        return value.toString();
+                      }}
+                      className="m-0 p-0"
+                    />
+                    <Tooltip
+                      contentStyle={{
+                        backgroundColor: "rgba(204, 204, 208, 0.7)",
+                        borderRadius: "2px",
+                        fontSize: "9px",
+                        fontWeight: 600,
+                        width: "90px",
+                        height: "30px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                      }}
+                      labelStyle={{ color: "#1f2937" }}
+                      formatter={(value) => [value, null]}
+                    />
+                    <Bar dataKey="revenue" fill="url(#revenueGradient)" radius={[1, 1, 0, 0]} />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+          )}
         </motion.div>
       )}
     </AnimatePresence>
