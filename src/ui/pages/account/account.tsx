4eddc2bbe9742a4d18fab9f854e3a60aedf6df93
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-21 14:32:20 +0000

    chore:

--- src/ui/pages/account/account.tsx
+++ src/ui/pages/account/account.tsx
@@ -1,9 +1,9 @@
-const Account = () => {
+const Settings = () => {
   return (
-    <div >
-      Account
+    <div className="rounded font-[550] text-7xl text-zinc-700 bg-zinc-200 flex items-enter justify-center items-center w-full h-full">
+      Under construction
     </div>
-  )
+  );
 };
 
-export default Account;
+export default Settings;
--- src/ui/pages/settings/settings.tsx
+++ src/ui/pages/settings/settings.tsx
@@ -1,9 +1,9 @@
 const Settings = () => {
   return (
-    <div >
-      Settings
+    <div className="rounded font-[550] text-7xl text-zinc-700 bg-zinc-200 flex items-enter justify-center items-center w-full h-full">
+      Under construction
     </div>
-  )
+  );
 };
 
 export default Settings;
