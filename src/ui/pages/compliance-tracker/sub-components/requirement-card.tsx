6dab3df81977d2de415c52f6d5d8c20bfaa041e7
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-23 01:29:02 +0000

    chore:

--- src/ui/pages/compliance-tracker/sub-components/requirement-card.tsx
+++ src/ui/pages/compliance-tracker/sub-components/requirement-card.tsx
@@ -1,6 +1,7 @@
 import getTextAndBackgroundColorFromString from "../../../../utils/get-text-and-background-color-from-string";
 import CheckedTasksIcon from "../../../components/icons/checked-tasks-icon";
 import UncheckedTasksIcon from "../../../components/icons/uncchecked-tasks-icon";
+import CloseIcon from "../../../components/icons/close-icon";
 
 const RequisiteCard = ({
   name,
@@ -30,7 +31,9 @@ const RequisiteCard = ({
           className={`${value ? "text-green-700" : "text-red-500"} transform translate-y-1 `}>
           {value ? <CheckedTasksIcon scale={0.75} /> : <UncheckedTasksIcon scale={0.75} />}
         </button>
-        <button onClick={deleteFunction}>delete</button>
+        <button onClick={deleteFunction} className="transform translate-y-1">
+          <CloseIcon scale={0.75} />
+        </button>
       </div>
     </div>
   );
